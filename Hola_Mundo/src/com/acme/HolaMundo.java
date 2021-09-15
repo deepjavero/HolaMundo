efc3d3658ee9113cb1b08ad98f16d8e73d492eb0
EAvila <eavila@bupa.com.mx>
2021-09-15 14:39:00 -0500

    Se agrego la calse hola mundo

---
+++ Hola_Mundo/src/com/acme/HolaMundo.java
@@ -0,0 +1,8 @@
+package com.acme;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        System.out.println("tabulador");
+    }
+}
